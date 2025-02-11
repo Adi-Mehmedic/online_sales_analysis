781509d5d3a5a156bdf07f3bdf99a00e621364b4
Adi-Mehmedic <fortnite.trashman@gmail.com>
2025-02-11 18:39:20 +0100

    Prikazana promjena kolicine proizvoda i ociscen prikaz funkcija

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ Tomato = Product("Tomato",1.3,10)
 Potato = Product("Potato",0.8,30)
 Banana = Product("Banana",1.8,25)
 Orange = Product("Orange",1.45,18)
-#Ispis produkta 
-print(Adi.show_products())
+
 #Dodavanje novog producta
 Adi.add_product(Product("Grapes",2.1,15))
 print(Adi.show_products())
-#Suma cijena
-print(Adi.suma_vrijednosti())
+
 #Dodavanje u korpu
 Korpa.add_items(Banana)
 Korpa.add_items(Orange)
@@ -24,4 +22,7 @@ Korpa.add_items(Potato)
 #Ispis naplate i sadrzaj korpe
 print(Korpa.naplata())
 print(Korpa.prikaz_korpe())
+#Promjena kolicina produckta
+Tomato.change_quantity(47)
+print(f"Tomato changed quantity: {Tomato.quantity}")
 
